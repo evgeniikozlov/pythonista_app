04f4c5eb2030f88bb1352a565906cf1f21e0c5a8
Evgenii Kozlov <evgenii_kozlov@epam.com>
2018-05-09 14:40:14 +0300

    grid settings

--- test.py
+++ test.py
@@ -36,35 +36,36 @@ class CalculationApp:
     def __init__(self):
         self.inputs = {}
 
-        view = ui.View(frame=(0, 0, 500, 500), name='Расчет укрытий', background_color='white')
+        view = ui.View(frame=(0, 0, 700, 500), name='Расчет укрытий', background_color='white')
         height = self.generate_height()
+        start_column = 30
 
         for content in self.content_map:
             label = ui.Label(text=content["header"])
-            label.center = (5, next(height))
+            label.center = (start_column, next(height))
             view.add_subview(label)
             for option in content["options"]:
                 switch = ui.Switch(value=False, name="{}_{}".format(content["header"], option), action=self.switch_pressed)
-                switch.center = (5, next(height))
+                switch.center = (start_column, next(height))
                 view.add_subview(switch)
                 textfield = ui.TextField(enabled=True, name="{}_{}".format(content["header"], option))
-                textfield.center = (20, next(height))
+                textfield.center = (start_column + 50, next(height))
                 view.add_subview(textfield)
                 label = ui.Label(text=option)
-                label.center = (50, next(height))
+                label.center = (start_column + 100, next(height))
                 view.add_subview(label)
         button = ui.Button(title='Расчет')
-        button.center = (200, 5)
+        button.center = (500, 50)
         button.action = self.calculate_pressed
         view.add_subview(button)
 
         button = ui.Button(title='Очистить')
-        button.center = (200, 30)
+        button.center = (500, 150)
         button.action = self.clear_inputs
         view.add_subview(button)
 
-        self.result_textfield = ui.TextField(enabled=True, name="result")
-        self.result_textfield.center = (200, 55)
+        self.result_textfield = ui.TextField(enabled=True, name="result", text="empty")
+        self.result_textfield.center = (500, 250)
         view.add_subview(self.result_textfield)
 
         view.present('sheet')
